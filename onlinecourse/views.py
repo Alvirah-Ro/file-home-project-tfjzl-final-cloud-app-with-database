d13c425b129f634e94efff1af0424cf15a275dcf
Alvirah-Ro <bajoraneh@hotmail.com>
2024-10-18 13:58:16 -0400

    minor changes in various places

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -117,7 +117,7 @@ def submit(request, course_id):
     submission = Submission.objects.create(enrollment=enrollment)
     choices = extract_answers(request)
     submission.choices.set(choices)
-    submission_id = submission_id
+    submission_id = submission.id
     return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(course_id, submission_id,)))
 
 def extract_answers(request):
@@ -146,7 +146,7 @@ def show_exam_result(request, course_id, submission_id):
 
     context['course'] = course
     context['choices'] = choices
-    context['grade'] = grade
+    context['grade'] = total_score
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 
